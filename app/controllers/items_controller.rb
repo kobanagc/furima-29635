cf6bfa72a7df3f92d44be40a2e797c32c6ce1cc5
kobanagc <naoki.kobayashi.0708@gmail.com>
2021-01-09 19:02:06 +0900

    if文任意修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,12 +37,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    item.destroy if item.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
